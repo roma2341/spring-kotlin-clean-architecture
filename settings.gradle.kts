ca0b0392c155f5cc47d6883e1209d75168c1f596
romaSHS <roman.z@outsource-to-ukraine.com>
2022-07-30 23:12:28 +0300

    basic version

---
+++ settings.gradle.kts
@@ -0,0 +1,8 @@
+
+rootProject.name = "crm-clean-architecture"
+include("domain")
+include("domain-api")
+include("domain-implementation")
+include("usecase-api")
+include("usecase-implementation")
+include("framework")
