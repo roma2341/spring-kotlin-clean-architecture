86d4feec5b4b5643a8fcaee81a91877b87741e02
Roman Zinchuk <zigzag2341@gmail.com>
2024-05-02 14:28:20 +0300

    Fixes

--- funnel_microservice/src/main/kotlin/com/zigzag/crm/framework/rest/user/UserRestClient.kt
+++ funnel_microservice/src/main/kotlin/com/zigzag/crm/framework/rest/user/UserRestClient.kt
@@ -1,4 +1,4 @@
-package com.zigzag.crm.framework.rest;
+package com.zigzag.crm.framework.rest.user;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping
